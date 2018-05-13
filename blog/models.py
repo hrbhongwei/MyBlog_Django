fc90ca92d0fe6c7466cb2b33fb255459d1f6afa1
hrbhongwei <hongwei_23@163.com>
2018-05-13 10:28:33 +0800

    详情页设计开始

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.utils.six import python_2_unicode_compatible
 
 
@@ -31,7 +32,7 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
-
+@python_2_unicode_compatible
 class Post(models.Model):
     """
     文章的数据库表稍微复杂一点，主要是涉及的字段更多。
@@ -68,8 +69,14 @@ class Post(models.Model):
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User)
 
+
     def __str__(self):
         return self.title
+
+    # 自定义 get_absolute_url 方法
+    # 记得从 django.urls 中导入 reverse 函数
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
 
 
 # # python_2_unicode_compatible 装饰器用于兼容 Python2
